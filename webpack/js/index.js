7dff56453b6ce2cb0392b8cde75684ed6ccca416
huang <1217160299@qq.com>
2017-03-08 18:24:18 +0800

    change2

--- webpack/js/index.js
+++ webpack/js/index.js
@@ -4,4 +4,9 @@ var boxnode=document.getElementById("box")
 boxnode.onmousedown=function(e){
 	var disx=e.clientX-boxnode.offsetLeft;
 	var disy=e.clientY-boxnode.offsetTop;
+	
+	document.onmousemove=function(e){
+		boxnode.offsetLeft=e.clientX-disx;
+		boxnode.offsetTop=e.clientY-disy;
+	}
 }
